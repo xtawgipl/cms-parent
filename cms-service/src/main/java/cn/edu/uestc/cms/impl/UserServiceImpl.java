9ea26fcc5dfc31e03e997060b9335e01c03165af
xtawgipl <xtawgipl1123@163.com>
2018-04-02 23:10:57 +0800

    更新

--- cms-service/src/main/java/cn/edu/uestc/cms/impl/UserServiceImpl.java
+++ cms-service/src/main/java/cn/edu/uestc/cms/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service("rserService")
+@Service("userService")
 public class UserServiceImpl implements UserService{
 
     @Resource
